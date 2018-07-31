54d696da07c59b8f75a7c2f548df4f30ad4d905c
Joel Barr <Joelbarr1220@gmail.com>
2018-07-31 08:21:02 -0600

    Got basics for login/register

--- Website/TravEx WebApp/SignUp.aspx.cs
+++ Website/TravEx WebApp/SignUp.aspx.cs
@@ -6,6 +6,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using TravEx_WebApp.App_Code;
 
+/***********************************
+ * This page written by Joel Barr
+ ***********************************/
 namespace TravEx_WebApp
 {
     public partial class SignUp : System.Web.UI.Page
@@ -47,19 +50,75 @@ namespace TravEx_WebApp
                 CustEmail = txtEmail.Text
             };
 
-            //int customerID = CustomerDB.InsertCustomer(customer);
+            int? customerID = CustomerDB.InsertCustomer(customer);
+
+            if (customerID == null)
+            {
+                throw new Exception("Cannot retrieve new customer ID");
+            }
 
             //Create Login
-            /*
-             * CustomerLogin login = new Login() {
-             *      CustomerID = customerID,
-             *      Password = txtPassword1.Text
-             * };
-             * */
-             
-            // Log in
+            CustomerLogin login = new CustomerLogin()
+            {
+                CustomerId = customerID.Value,
+                Password = txtPassword1.Text
+            };
 
+            CustomerDB.InsertLogin(login);
+
+            // Log in
+            Login(customerID.Value);
         }
 
+        private void Login(int customerID)
+        {
+            Session["CustomerId"] = customerID;
+            Server.Transfer("MyAccount.aspx", true);
+        }
+
+        protected void loginEmailvalidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            args.IsValid = false;
+            
+            int? customerId = GetLoginID();
+
+            if (customerId != null && CustomerDB.LoginExists(customerId.Value))
+                args.IsValid = true;
+        }
+
+        private int? GetLoginID()
+        {
+            if (txtLoginEmail.Text == String.Empty) return null;
+
+            Customer customer = CustomerDB.GetCustomerByEmail(txtLoginEmail.Text);
+            if (customer == null) return null;
+
+            int customerId = customer.CustomerId;
+            return customerId;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            int? customerID = GetLoginID();
+            if (customerID == null || !CustomerDB.CheckPassword(customerID.Value, txtLoginPassword.Text)) return;
+
+            Login(customerID.Value);
+        }
+
+        protected void loginPasswordValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            args.IsValid = false;
+            string pwInput;
+
+            if (txtLoginPassword.Text == String.Empty)
+                return;
+            else
+                pwInput = txtLoginPassword.Text;
+
+            int? customerID = GetLoginID();
+            
+            if (customerID != null && CustomerDB.CheckPassword(customerID.Value, pwInput))
+                args.IsValid = true;
+        }
     }
 }
